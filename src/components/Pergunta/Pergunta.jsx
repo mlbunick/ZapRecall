c4011bbe198ab1f0d27d377111d6c1934bc0a2f6
Matheus Leal <mlbunick85@gmail.com>
2025-05-06 21:09:40 -0300

    Commit Inicial

---
+++ src/components/Pergunta/Pergunta.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "../Pergunta.cs"
+
+export default function Pergunta({ titulo, pergunta }){
+    const [estado, setEstado] = useState("fechada");
+
+    function abrirPergunta() {
+        setEstado("aberta");
+    }
+    
+    return (
+        <div className="pergunta-container">
+          {estado === "fechada" ? (
+            <div className="pergunta-fechada" onClick={abrirPergunta}>
+              <p>{titulo}</p>
+            </div>
+          ) : 
+          (
+            <div className="pergunta-aberta">
+              <p>{pergunta}</p>
+            </div>
+          )}
+        </div>
+      );
+}
